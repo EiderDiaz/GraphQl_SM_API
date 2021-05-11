d63c99ff9aa4e96ec14dcefff8074a3b14f2e8f7
eiderdiazmoraila@hotail.com <eiderdiazmoraila@hotmail.com>
2021-05-11 13:41:47 -0600

    added Post model, created its GraphQL input,types and mutator

--- graphql_sm_api/schema.py
+++ graphql_sm_api/schema.py
@@ -8,18 +8,29 @@ class User(DjangoObjectType):
     class Meta:
         model = models.User
 
+
+class Post(DjangoObjectType):
+    class Meta:
+        model = models.Post
+
+
 class UserInput(graphene.InputObjectType):
     name = graphene.String()
+
+class PostInput(graphene.InputObjectType):
+    content = graphene.String()
+    user_id = graphene.Int()
 
 class CreateUser(graphene.Mutation):
     class Arguments:
         input = UserInput(required=True)
+
     ok = graphene.Boolean()
     user = graphene.Field(User)
 
     @staticmethod
     def mutate(root, info, input):
-        instance = models.User(name=input.name,)
+        instance = models.User(name=input.name, )
 
         try:
             instance.save()
@@ -28,10 +39,31 @@ class CreateUser(graphene.Mutation):
         instance.followers.set([])
         return CreateUser(ok=True, user=instance)
 
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        input = PostInput(required=True)
+
+    ok = graphene.Boolean()
+    post = graphene.Field(Post)
+
+    @staticmethod
+    def mutate(root, info, input):
+        user = models.User.objects.get(pk=input.user_id)
+        if not models.User.objects.filter(pk=user.pk).exists():
+            return CreatePost(ok=False, post=None)
+
+        instance = models.Post(content=input.content, created_by=user)
+        try:
+            instance.save()
+        except Exception:
+            return CreatePost(ok=False, post=None)
+
+        return CreatePost(ok=True, post=instance)
 
 class Query(graphene.ObjectType):
     # hello = graphene.String(default_value="Hi!")
     user = graphene.Field(User, id=graphene.Int())
+    users = graphene.List(User)
 
     def resolve_user(self, info, **kwargs):
         id = kwargs.get("id")
@@ -39,8 +71,13 @@ class Query(graphene.ObjectType):
             return models.User.objects.get(pk=id)
         else:
             return None
+    def resolve_users(self, info, **kwargs):
+        return models.User.objects.all()
+
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
